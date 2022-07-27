4be2d5fe684d227060923a07027884ca57c5a755
Glitch (ilhanilhan2312) <none>
2022-07-27 20:47:57 +0000

    🚒🍁 Checkpoint ./events/ready.js:39250639/4

--- events/ready.js
+++ events/ready.js
@@ -8,7 +8,7 @@ module.exports = async client => {
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
 
     client.user.setActivity(oyun[random], { type: "WATCHING" });
-  }, 13000);
+  }, 10000);
   client.user.setStatus("dnd");
 
 }
