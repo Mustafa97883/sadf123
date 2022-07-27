ebfcc1dcc720b9c3031255dc576c3ef2a3f8ddf2
Glitch (ilhanilhan2312) <none>
2022-07-27 21:23:59 +0000

    👋👢 Checkpoint ./komutlar/yardım-nsfw.js:39250639/560

--- komutlar/yardım-nsfw.js
+++ komutlar/yardım-nsfw.js
@@ -2,7 +2,10 @@ const Discord = require('discord.js');
 const data = require('quick.db');
 
 exports.run = async (client, message, args) => {
-
+const DBL = require('dblapi.js')
+const dbl = new DBL('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Ijc1Njg4MzMwOTI3MDY2MzIyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNjU2MTEwOTkxfQ.7Oqg1lelprL5ACm4Yh0RKREKaOTPIyQRrSjDaT7uKko', client)
+dbl.hasVoted(message.author.id).then(voted => {
+      if(voted) {
 message.channel.send(new Discord.MessageEmbed().setTitle(`🔞`).setDescription(`
 **
 s!anal
@@ -15,7 +18,13 @@ s!pussy
 s!thigh
 **`));
 
-};
+} else {
+        message.channel.send(` Bu Komutu Sadece 12 Saatte Bir Oyvererek Kullanabilirsiniz Oyvermek İçin (https://top.gg/bot/756883309270663229/vote) linke Tıklayarak Oyverebilirsiniz. Oy Verdiyseniz 5 Dakka Bekleyiniz`) 
+              
+}
+        })
+      
+      },
 exports.conf = {
   enabled: true,
   guildOnly: true,
